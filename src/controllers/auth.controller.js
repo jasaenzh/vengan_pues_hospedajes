f4cf54b639f873509a735dc5aff0b14daa937c6a
Jhony Saenz <jash0310@gmail.com>
2023-07-10 22:25:08 -0500

    agregando domain 7

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -116,7 +116,7 @@ export const loginUser = async (req, res) => {
     /** Creo el token pasandole el id del usuario  */
     const token = await generateToken({ id: findUser._id })
 
-    console.log(token)
+    console.log("TOKEN:", token)
 
     /** Guardo el token en la cookie *///
     res.cookie('token', token, {
