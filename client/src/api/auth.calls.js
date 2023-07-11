20ad8db3d7ff7d5f6a0081d50aaed1379c54b38e
Jhony Saenz <jash0310@gmail.com>
2023-07-10 23:50:50 -0500

    arreglando cookie 4

--- client/src/api/auth.calls.js
+++ client/src/api/auth.calls.js
@@ -4,10 +4,6 @@ export const registerRequest = (user) => axios.post(`/register`, user)
 
 export const verifyEmailRequest = (emailVerificationCode) => axios.post(`/verify-email`, { emailVerificationCode })
 
-export const loginRequest = (user) => axios.post(`/login`, user, {
-  withCredentials: true,
-  credentials: "include",
-  headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }
-})
+export const loginRequest = (user) => axios.post(`/login`, user)
 
 export const profileRequest = () => axios.get(`/profile`)
