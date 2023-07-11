7d9156902b274a427eb6d604c9503d0ed692cef1
Jhony Saenz <jash0310@gmail.com>
2023-07-11 00:36:59 -0500

    arreglando cookie 8

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use(cors({
     'http://localhost:3000',
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(morgan("dev"));
