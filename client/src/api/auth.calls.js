484b4a40293eb183dd01abf3d17ea71478287f4e
Jhony Saenz <jash0310@gmail.com>
2023-07-10 20:58:58 -0500

    agregando header

--- client/src/api/auth.calls.js
+++ client/src/api/auth.calls.js
@@ -4,6 +4,9 @@ export const registerRequest = (user) => axios.post(`/register`, user)
 
 export const verifyEmailRequest = (emailVerificationCode) => axios.post(`/verify-email`, { emailVerificationCode })
 
-export const loginRequest = (user) => axios.post(`/login`, user)
+export const loginRequest = (user) => axios.post(`/login`, user, {
+  withCredentials: true,
+  headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }
+})
 
 export const profileRequest = () => axios.get(`/profile`)
