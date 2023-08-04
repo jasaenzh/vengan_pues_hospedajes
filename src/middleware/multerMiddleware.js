67fe72a8027990f5bfd90484a79735e5f4140ded
Jhony A Saenz <jash0310@gmail.com>
2023-08-04 18:28:14 -0500

    Arreglando problema Internal Server al crear un apartamento

--- src/middleware/multerMiddleware.js
+++ src/middleware/multerMiddleware.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads');
+    cb(null, './imgUploads');
   },
   filename: (req, file, cb) => {
     const fileExtension = file.originalname.split('.').pop();
